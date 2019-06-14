75e7955dd3d207cd994499ff98f86590df6a2aeb
Ren Rizzolo <renrizmusic@gmail.com>
2019-06-14 16:07:50 +1000

    home page

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -3,25 +3,25 @@ import { Component, h } from '@stencil/core';
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.css',
-  shadow: true
+  shadow: true,
 })
 export class AppHome {
-
   render() {
     return (
-      <div class='app-home'>
-        <p>
-          Welcome to the Stencil App Starter.
-          You can use this starter to build entire apps all with
-          web components using Stencil!
-          Check out our docs on <a href='https://stenciljs.com'>stenciljs.com</a> to get started.
-        </p>
+      <div class="app-home">
+        <app-background>
+          <renrizzolo-logo />
+          <p class="intro-text">I'm a front-end developer living in Melbourne, Australia ✌🏼</p>
+          <div class="wave" />
+          <div class="wave" />
+          <div class="wave" />
 
-        <stencil-route-link url='/profile/stencil'>
-          <button>
-            Profile page
-          </button>
-        </stencil-route-link>
+          <stencil-route-link url="/profile/ren">link</stencil-route-link>
+          <div class="footer">
+            <a href="https://twitter.com/ren_riz">twitter</a>&nbsp;
+            <a href="https://github.com/renrizzolo">gitHub</a>
+          </div>
+        </app-background>
       </div>
     );
   }
