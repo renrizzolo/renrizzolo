431762eddf79a9f5dcecf9a3377e2b9f806c4487
renrizzolo <renrizmusic@gmail.com>
2019-06-17 00:15:32 +1000

    add transition styles to switch

--- src/components/app-root/app-root.e2e.ts
+++ src/components/app-root/app-root.e2e.ts
@@ -8,10 +8,4 @@ describe('app-root', () => {
     expect(element).toHaveClass('hydrated');
   });
 
-  it('renders the title', async () => {
-    const page = await newE2EPage({ url: '/'});
-
-    const element = await page.find('app-root >>> h1');
-    expect(element.textContent).toEqual('Stencil App Starter');
-  });
 });
--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h,  } from '@stencil/core';
 
 @Component({
   tag: 'app-root',
@@ -6,16 +6,51 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class AppRoot {
+
+
   render() {
     return (
       <div>
         <main>
           <stencil-router>
-            <stencil-route-switch scrollTopOffset={0}>
-              <stencil-route url="/" component="app-home" exact={true} />
-              <stencil-route url="/profile/:name" component="app-profile" />
-              <stencil-route component="catch-404" />
-            </stencil-route-switch>
+            <stencil-route routeRender={({ history: { location } }) => (
+              <div>
+              <stencil-route-redirect url="/" />
+              <route-transition
+                items={location}
+                keys={location.key}
+                config={{ duration: 800, timing: 'cubic-bezier(0.63, 1, 0.620, 1)' }} //cubic-bezier(0.23, 1, 0.320, 1)
+                from={{  opacity: '0', transform: 'translateY(15px) scale(1.05)' }}
+                enter={{ opacity: '1',   transform: 'translateY(0px) scale(1)'  }}
+                leave={{ opacity: '0', transform: 'translateY(15px) scale(1.05)' }}
+                renderFunction={(style, loc ) => {
+                return (
+
+                  <stencil-route-switch location={loc ? {...loc} : location} scrollTopOffset={0}>
+                    <stencil-route url="/" component="app-home" exact={true} componentProps={style} 
+                      routeRender={(props) => (
+                        <app-home 
+                          {...props} 
+                          styles={style}
+                        />
+                      )}
+                    />
+                   <stencil-route url="/profile/:name" component="app-profile" componentProps={style} 
+                    routeRender={(props) => (
+                      <app-profile 
+                          {...props}
+                          styles={style} 
+                        />
+                      )} 
+                    /> 
+                    <stencil-route component="catch-404" componentProps={style} routeRender={() => (<catch-404 />)} />
+                  </stencil-route-switch>
+                )}}
+              />
+              </div> 
+              )}
+            />
+            
           </stencil-router>
         </main>
       </div>
