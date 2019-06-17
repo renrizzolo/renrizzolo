e2b941db87e16099cd8ad33908ac0aef6012f216
renrizzolo <renrizmusic@gmail.com>
2019-06-17 23:21:02 +1000

    ad a configurable css grid

---
+++ src/components/ui-grid/ui-grid.tsx
@@ -0,0 +1,22 @@
+import { Component, h, Prop, } from '@stencil/core';
+
+
+@Component({
+  tag: 'ui-grid',
+  styleUrl: 'ui-grid.css',
+  shadow: true,
+})
+export class UiGrid {
+  @Prop() 
+  class?: string;
+  @Prop() 
+  cols: 1 | 2 | 3 | 4 = 1;
+  @Prop() 
+  gap: 0 | 1 | 2 | 3 | 4 = 0;
+
+  render() {
+    return <div class={`grid grid--cols-${this.cols} grid--gap-${this.gap} ${this.class}`}>
+      <slot/>
+      </div>
+  }
+}
