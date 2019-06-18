7467626638eae95d884ee824c18ae41f5379dc22
Ren Rizzolo <renrizmusic@gmail.com>
2019-06-18 16:37:34 +1000

    fix route transitions being weird

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -1,4 +1,4 @@
-import { Component, h,  } from '@stencil/core';
+import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'app-root',
@@ -6,59 +6,55 @@ import { Component, h,  } from '@stencil/core';
   shadow: true,
 })
 export class AppRoot {
-
-
   render() {
     return (
       <div>
         <main>
           <stencil-router>
-            <stencil-route routeRender={({ history: { location } }) => (
-              <div>
-              <stencil-route-redirect url="/" />
-              <route-transition
-                items={location}
-                keys={location.key}
-                config={{ duration: 800, timing: 'cubic-bezier(0.63, 1, 0.620, 1)' }}
-                from={{  opacity: '0', transform: 'translateY(15px) scale(1.05)' }}
-                enter={{ opacity: '1',   transform: 'translateY(0px) scale(1)'  }}
-                leave={{ opacity: '0', transform: 'translateY(15px) scale(1.05)' }}
-                renderFunction={(style, loc ) => {
-                return (
-
-                  <stencil-route-switch location={loc ? {...loc} : location} scrollTopOffset={0}>
-                    <stencil-route url="/" component="app-home" exact={true} componentProps={style} 
-                      routeRender={(_) => (
-                        <div style={style}>
-                          <app-home 
-                            
+            <stencil-route
+              routeRender={({ history: { location } }) => (
+                <div>
+                  <stencil-route-redirect url="/" />
+                  <route-transition
+                    items={location}
+                    keys={location.key}
+                    config={{ duration: 600, timing: 'cubic-bezier(0.63, 1, 0.620, 1)' }}
+                    from={{ opacity: '0', transform: 'translateY(15px) scale(1.05)' }}
+                    enter={{ opacity: '1', transform: 'translateY(0px) scale(1)' }}
+                    leave={{ opacity: '0', transform: 'translateY(15px) scale(1.05)' }}
+                    renderFunction={(style, loc) => {
+                      return (
+                        <stencil-route-switch
+                          location={loc ? { ...loc } : location}
+                          scrollTopOffset={0}
+                        >
+                          <stencil-route
+                            url="/"
+                            component="app-home"
+                            exact={true}
+                            componentProps={{ styles: style }}
                           />
-                        </div>
-                      )}
-                    />
-                   <stencil-route url="/projects" componentProps={style} 
-                    routeRender={(_) => (
-                      <div style={style}>
-                      <app-page-projects 
-                          
-                        />
-                        </div>
-                      )} 
-                    /> 
-                    <stencil-route componentProps={style} 
-                      routeRender={(_) => (
-                        <div style={style}>
-                          <catch-404 />
-                        </div>
-                      )} 
-                    />
-                  </stencil-route-switch>
-                )}}
-              />
-              </div> 
+                          <stencil-route
+                            url="/projects"
+                            component="app-page-projects"
+                            exact={true}
+                            componentProps={{ styles: style }}
+                          />
+                          <stencil-route
+                            componentProps={{ styles: style }}
+                            routeRender={(_) => (
+                              <transition-mount-wrapper mounted={true} styles={style}>
+                                <app-page-404 />
+                              </transition-mount-wrapper>
+                            )}
+                          />
+                        </stencil-route-switch>
+                      );
+                    }}
+                  />
+                </div>
               )}
             />
-            
           </stencil-router>
         </main>
       </div>
