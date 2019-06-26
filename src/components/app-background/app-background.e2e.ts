9a9ad8a26cdf3b50a98edcd07c1682e2432aa10c
Ren Rizzolo <renrizmusic@gmail.com>
2019-06-26 13:23:36 +1000

    remove test

--- src/components/app-background/app-background.e2e.ts
+++ src/components/app-background/app-background.e2e.ts
@@ -8,12 +8,4 @@ describe('app-background', () => {
     const element = await page.find('app-background');
     expect(element).toHaveClass('hydrated');
   });
-
-  it('contains a "Profile Page" button', async () => {
-    const page = await newE2EPage();
-    await page.setContent('<app-background></app-background>');
-
-    const element = await page.find('app-background >>> button');
-    expect(element.textContent).toEqual('Profile page');
-  });
 });
