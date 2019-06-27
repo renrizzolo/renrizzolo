334dd9963a7dfcf549bb6f08888f3068866cf8fa
Ren Rizzolo <renrizmusic@gmail.com>
2019-06-27 13:54:17 +1000

    hmm

--- src/components/app-page-project/app-page-project.tsx
+++ src/components/app-page-project/app-page-project.tsx
@@ -38,69 +38,69 @@ export class AppPageProject {
   //   console.log('will update match', this.match);
   // }
   render() {
-    console.log('this.isMounted', this.isMounted);
-    console.log('this.styles', this.styles);
-
     if (this.match && this.match.params.slug) {
       this.project = projects.filter((proj) => proj.slug === this.match.params.slug)[0];
-      this.next = projects.filter((proj) => proj.id === this.project.id + 1)[0];
-      this.prev = projects.filter((proj) => proj.id === this.project.id - 1)[0];
+      this.next = this.project
+        ? projects.filter((proj) => proj.id === this.project.id + 1)[0]
+        : null;
+      this.prev = this.project
+        ? projects.filter((proj) => proj.id === this.project.id - 1)[0]
+        : null;
 
-      console.log(this.project);
-      if (this.project) {
-        return (
-          <div class="app-page-project">
-            <app-background>
-              <app-wave class="wave--flipped" />
+      return (
+        <div class="app-page-project">
+          <app-background>
+            <app-wave class="wave--flipped" />
 
-              <ui-container>
-                <ui-button url="/projects/" class="abs abs--top-left">
-                  Back
+            <ui-container>
+              <ui-button url="/projects/" class="abs abs--top-left">
+                Back
+              </ui-button>
+              {this.next && (
+                <ui-button
+                  url={`/project/${this.next.slug}`}
+                  class="link-next color--alt abs abs--right abs--middle flex-center"
+                >
+                  <svg
+                    version="1.1"
+                    id="Layer_1"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 50 50"
+                    style={{ width: '18', height: '18' }}
+                  >
+                    <path
+                      d="M48,25c0-0.6-0.2-1.1-0.6-1.4l0,0L37,13.3l0,0c-0.4-0.4-0.9-0.6-1.4-0.6c-1.1,0-2,0.9-2,2c0,0.6,0.2,1.1,0.6,1.4l0,0
+	l6.9,6.9H4c-1.1,0-2,0.9-2,2s0.9,2,2,2h37.2l-6.9,6.9l0,0c-0.4,0.4-0.6,0.9-0.6,1.4c0,1.1,0.9,2,2,2c0.6,0,1.1-0.2,1.4-0.6l0,0
+	l10.3-10.3l0,0C47.8,26.1,48,25.6,48,25z"
+                      fill="currentColor"
+                    />
+                  </svg>
                 </ui-button>
-                {this.next && (
-                  <ui-button
-                    url={`/project/${this.next.slug}`}
-                    class="link-next color--alt abs abs--right abs--middle flex-center"
+              )}
+              {this.prev && (
+                <ui-button
+                  url={`/project/${this.prev.slug}`}
+                  class="link-prev color--alt abs abs--left  abs--middle flex-center"
+                >
+                  <svg
+                    version="1.1"
+                    id="Layer_1"
+                    x="0px"
+                    y="0px"
+                    viewBox="0 0 50 50"
+                    style={{ width: '18', height: '18', transform: 'rotate(180deg)' }}
                   >
-                    <svg
-                      version="1.1"
-                      id="Layer_1"
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 50 50"
-                      style={{ width: '18', height: '18' }}
-                    >
-                      <path
-                        d="M48,25c0-0.6-0.2-1.1-0.6-1.4l0,0L37,13.3l0,0c-0.4-0.4-0.9-0.6-1.4-0.6c-1.1,0-2,0.9-2,2c0,0.6,0.2,1.1,0.6,1.4l0,0
+                    <path
+                      d="M48,25c0-0.6-0.2-1.1-0.6-1.4l0,0L37,13.3l0,0c-0.4-0.4-0.9-0.6-1.4-0.6c-1.1,0-2,0.9-2,2c0,0.6,0.2,1.1,0.6,1.4l0,0
 	l6.9,6.9H4c-1.1,0-2,0.9-2,2s0.9,2,2,2h37.2l-6.9,6.9l0,0c-0.4,0.4-0.6,0.9-0.6,1.4c0,1.1,0.9,2,2,2c0.6,0,1.1-0.2,1.4-0.6l0,0
 	l10.3-10.3l0,0C47.8,26.1,48,25.6,48,25z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </ui-button>
-                )}
-                {this.prev && (
-                  <ui-button
-                    url={`/project/${this.prev.slug}`}
-                    class="link-prev color--alt abs abs--left  abs--middle flex-center"
-                  >
-                    <svg
-                      version="1.1"
-                      id="Layer_1"
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 50 50"
-                      style={{ width: '18', height: '18', transform: 'rotate(180deg)' }}
-                    >
-                      <path
-                        d="M48,25c0-0.6-0.2-1.1-0.6-1.4l0,0L37,13.3l0,0c-0.4-0.4-0.9-0.6-1.4-0.6c-1.1,0-2,0.9-2,2c0,0.6,0.2,1.1,0.6,1.4l0,0
-	l6.9,6.9H4c-1.1,0-2,0.9-2,2s0.9,2,2,2h37.2l-6.9,6.9l0,0c-0.4,0.4-0.6,0.9-0.6,1.4c0,1.1,0.9,2,2,2c0.6,0,1.1-0.2,1.4-0.6l0,0
-	l10.3-10.3l0,0C47.8,26.1,48,25.6,48,25z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </ui-button>
-                )}
+                      fill="currentColor"
+                    />
+                  </svg>
+                </ui-button>
+              )}
+              {this.project ? (
                 <transition-group
                   from={{ opacity: '0' }}
                   enter={{ opacity: '1' }}
@@ -110,13 +110,17 @@ export class AppPageProject {
                   keys={(item) => (item.$ ? item.$.key : item.$key$)}
                   items={[<project-item key={this.project.id} post={this.project} fullSize />]}
                 />
-              </ui-container>
-            </app-background>
-          </div>
-        );
-      } else {
-        return <app-page-404 />;
-      }
+              ) : (
+                <h1>
+                  <code>404: not found.</code>
+                </h1>
+              )}
+            </ui-container>
+          </app-background>
+        </div>
+      );
+    } else {
+      return <app-page-404 />;
     }
   }
 }
