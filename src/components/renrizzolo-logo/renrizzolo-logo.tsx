80cde4d0de2800b845aa8961ac224611330b2603
renrizzolo <renrizmusic@gmail.com>
2019-06-27 22:39:14 +1000

    nvm

--- src/components/renrizzolo-logo/renrizzolo-logo.tsx
+++ src/components/renrizzolo-logo/renrizzolo-logo.tsx
@@ -3,7 +3,7 @@ import { Component, h } from '@stencil/core';
 @Component({
   tag: 'renrizzolo-logo',
   styleUrl: 'renrizzolo-logo.css',
-  shadow: true,
+  shadow: false,
 })
 export class RenrizzoloLogo {
   render() {
