53ffd3eef85adf1947b522d4f25424dbc95dd929
renrizzolo <renrizmusic@gmail.com>
2019-07-02 22:35:56 +1000

    maybe fix lottie prerender issues

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,11 +1,27 @@
 import { Config } from '@stencil/core';
 import { postcss } from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
+import nodePolyfills from 'rollup-plugin-node-builtins';
+import autoExternal from 'rollup-plugin-auto-external';
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  rollupConfig: {
+    outputOptions: {
+      // globals: {
+      //   canvas: 'canvas',
+      // }
+    }
+  },
   plugins: [
+    
+    // nodePolyfills(),
+    // autoExternal({
+    //   // builtins: false,
+    //   // dependencies: false,
+      
+    // }),
     postcss({
       plugins: [
         autoprefixer({
