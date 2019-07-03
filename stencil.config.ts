d80054c624c0b43345335366503bf1a6e85e6d58
Ren Rizzolo <renrizmusic@gmail.com>
2019-07-03 11:06:53 +1000

    upload local build to test on netlify w/ canvas hack in stencil/hydrate

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,27 +1,11 @@
 import { Config } from '@stencil/core';
 import { postcss } from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
-import nodePolyfills from 'rollup-plugin-node-builtins';
-import autoExternal from 'rollup-plugin-auto-external';
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  rollupConfig: {
-    outputOptions: {
-      // globals: {
-      //   canvas: 'canvas',
-      // }
-    }
-  },
   plugins: [
-    
-    nodePolyfills(),
-    // autoExternal({
-    //   // builtins: false,
-    //   // dependencies: false,
-      
-    // }),
     postcss({
       plugins: [
         autoprefixer({
@@ -30,7 +14,7 @@ export const config: Config = {
       ],
     }),
   ],
-  copy: [{ src: '_redirects' }],
+  copy: [{ src: '_redirects' }], // netlify SPA redirects
   globalStyle: 'src/global/app.css',
   globalScript: 'src/global/app.ts',
   outputTargets: [
@@ -38,7 +22,7 @@ export const config: Config = {
       type: 'www',
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      prerenderConfig: './prerender.config.js',
+      // prerenderConfig: './prerender.config.js',
       baseUrl: 'https://elastic-hodgkin-a279cc.netlify.com/',
     },
   ],
