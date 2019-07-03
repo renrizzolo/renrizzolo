db14026772bd759fe95043e57e7697283ec20d69
Ren Rizzolo <renrizmusic@gmail.com>
2019-07-03 17:05:44 +1000

    finish api, move image upload

--- server/app.js
+++ server/app.js
@@ -1,61 +1,34 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var multer = require("multer");
-var indexRouter = require("./routes/index");
-var postRouter = require("./routes/post");
-var Api = require("./api");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var multer = require('multer');
+var indexRouter = require('./routes/index');
+var postRouter = require('./routes/post');
+var Api = require('./api');
 var app = express();
 
 const api = new Api();
 console.log(api.updateProject);
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-var storage = multer.diskStorage({
-  destination: function(req, file, next) {
-    next(null, "./src/assets/uploads");
-  },
-  filename: function(req, file, next) {
-    next(null, file.originalname + ".jpg");
-  }
-});
-var upload = multer({ storage: storage });
-var fUpload = upload.fields([{ name: "image", maxCount: 1 }]);
-app.post("/api/update", api.updateProject);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.post("/api/upload", function(req, res, next) {
-  // upload
-  fUpload(req, res, function(err) {
-    // File details
-    console.log(req.files.image[0].filename);
-    // console.log(req.image);
+app.post('/api/create', api.createProject);
+app.post('/api/update', api.updateProject);
+app.post('/api/delete', api.deleteProject);
+app.post('/api/upload', api.uploadImage);
 
-    if (err) {
-      console.error(err);
-      console.log("An error occurred when uploading");
-    } else {
-      res.send({
-        image: "/assets/uploads/" + req.files.image[0].filename
-      });
-    }
-  });
-});
-
-app.use("/", indexRouter);
-app.use("/post", postRouter);
-
-// app.post("/api/create", api.createProject);
-// app.post('/api/delete', api.deleteProject)
+app.use('/', indexRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -66,11 +39,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
