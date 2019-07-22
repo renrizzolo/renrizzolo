2da44c02415d2ebfc044832aa706543b01ac92a4
Ren Rizzolo <renrizmusic@gmail.com>
2019-07-22 16:18:25 +1000

    server updates

--- server/app.js
+++ server/app.js
@@ -1,33 +1,38 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var indexRouter = require("./routes/index");
-var postRouter = require("./routes/post");
-var Api = require("./api");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var postRouter = require('./routes/post');
+var Api = require('./api');
+
+// this is just used for displaying
+// the current cover image on the server
+process.env['url'] = 'https://elastic-hodgkin-a279cc.netlify.com/';
 
 var app = express();
 
 var api = new Api();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../src/')));
 
-app.post("/api/create", api.createProject);
-app.post("/api/update", api.updateProject);
-app.post("/api/delete", api.deleteProject);
-app.post("/api/upload", api.uploadImage);
+app.post('/api/create', api.createProject);
+app.post('/api/update', api.updateProject);
+app.post('/api/delete', api.deleteProject);
+app.post('/api/upload', api.uploadImage);
 
-app.use("/", indexRouter);
-app.use("/post", postRouter);
+app.use('/', indexRouter);
+app.use('/post', postRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,11 +43,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
