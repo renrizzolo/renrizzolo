2d74b05983874e12a5de739e94830ccfcea3d215
Ren Rizzolo <renrizmusic@gmail.com>
2019-08-20 10:41:14 +1000

    add email to footer

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -20,15 +20,21 @@ export class AppHome implements ComponentInterface {
             <renrizzolo-logo />
             <div class="intro-text">
               <p>I'm a front-end developer from Melbourne, Australia ✌️</p>
+ 
               <div role="navigation" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
                 <ui-button itemprop="url" button url="/projects">
                   Projects
                 </ui-button>
               </div>
+               
             </div>
             </header>
 
             <div class="footer">
+              <ui-button button href="mailto:info@renrizzolo.com">
+                Email
+              </ui-button>
+              &nbsp;
               <ui-button button href="https://twitter.com/ren_riz">
                 Twitter
               </ui-button>
@@ -36,6 +42,7 @@ export class AppHome implements ComponentInterface {
               <ui-button button href="https://github.com/renrizzolo">
                 GitHub
               </ui-button>
+            
             </div>
           </div>
         </app-background>
