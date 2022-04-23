39c2813ac0237e03a0a6e06fbb5f81fd04691ba9
renrizzolo <renrizmusic@gmail.com>
2022-04-23 12:28:08 +1000

    fix: add gtgc, cleanup & build

--- src/components/project-item/project-item.tsx
+++ src/components/project-item/project-item.tsx
@@ -1,15 +1,15 @@
-import { Component, h, Prop, State } from '@stencil/core';
-import { Project } from '../app-page-projects/projects';
-import arrow from '../../icons/arrow.json';
+import { Component, h, Prop, State } from "@stencil/core";
+import { Project } from "../app-page-projects/projects";
+import arrow from "../../icons/arrow.json";
 
 @Component({
-  tag: 'project-item',
-  styleUrl: 'project-item.css',
+  tag: "project-item",
+  styleUrl: "project-item.css",
   shadow: true,
 })
 export class ProjectItem {
   @Prop()
-  type: string = '';
+  type: string = "";
   @Prop()
   post: Project;
   @Prop()
@@ -43,7 +43,6 @@ export class ProjectItem {
         class={`project-item--container ${this.type}`}
         key={`post-${post.id}`}
       >
-    
         {post.coverImage && (
           <div class="project-item--image__wrapper">
             {!this.type ? (
@@ -54,7 +53,11 @@ export class ProjectItem {
                   itemtype="https://schema.org/ImageObject"
                   class="project-item--figure"
                 >
-                  <img class="project-item--image" alt={post.heading} src={post.coverImage} />
+                  <img
+                    class="project-item--image"
+                    alt={post.heading}
+                    src={post.coverImage}
+                  />
                 </figure>
               </stencil-route-link>
             ) : (
@@ -64,21 +67,24 @@ export class ProjectItem {
                   itemtype="https://schema.org/ImageObject"
                   class="project-item--figure"
                 >
-                  
-                  <img class="project-item--image" alt={post.heading} src={post.coverImage} />
+                  <img
+                    class="project-item--image"
+                    alt={post.heading}
+                    src={post.coverImage}
+                  />
                 </figure>
               </div>
             )}
           </div>
         )}
-        <div class={'project-item--heading__overlay'}>
-         
-          </div>
+        <div class={"project-item--heading__overlay"}></div>
         <svg class="wave-clip-svg" width="0" height="0">
           <defs>
             <clipPath id="wave-clip" clipPathUnits="objectBoundingBox">
-              <path transform="scale(0.0013, 0.00254)"
-                d="M0,109.5c0,0,96.9-26.7,329.5,6.6C492,139.4,625.1,160,775.7,150.8c144.9-8.9,248.3-33.7,248.3-33.7V512H0L0,109.5z" />
+              <path
+                transform="scale(0.0013, 0.00254)"
+                d="M0,109.5c0,0,96.9-26.7,329.5,6.6C492,139.4,625.1,160,775.7,150.8c144.9-8.9,248.3-33.7,248.3-33.7V512H0L0,109.5z"
+              />
             </clipPath>
           </defs>
         </svg>
@@ -87,7 +93,7 @@ export class ProjectItem {
             <ui-button
               button
               title={`Previous: ${this.prev.heading}`}
-              iconPos={'left'}
+              iconPos={"left"}
               icon={arrow}
               url={`/project/${this.prev.slug}`}
               class="link-prev abs abs--left flex-center"
@@ -110,7 +116,7 @@ export class ProjectItem {
               <div class="project-item--preview-container next">
                 <project-item type="preview" post={this.next} mounted={true} />
               </div>
-              </ui-button>
+            </ui-button>
           )}
           <div class="tags-container">
             {post.tags &&
@@ -119,45 +125,58 @@ export class ProjectItem {
                   #{tag}
                 </span>
               ))}
-          </div>          
-          {this.type !== 'full-size' && (
+          </div>
+          {this.type !== "full-size" && (
             <transition-group
-              config={{ duration: 250, timing: 'woosh', delay: 150 }}
+              config={{ duration: 250, timing: "woosh", delay: 150 }}
               mounted={this.ready}
-              from={{ opacity: '0', transform: 'translateX(7px)' }}
-              enter={{ opacity: '1', transform: 'translateX(0px)' }}
-              leave={{ opacity: '0', transform: 'translateX(7px)' }}
+              from={{ opacity: "0", transform: "translateX(7px)" }}
+              enter={{ opacity: "1", transform: "translateX(0px)" }}
+              leave={{ opacity: "0", transform: "translateX(7px)" }}
               items={[
-                <h1 class="project-item--heading">
-                  {!this.type ? <stencil-route-link url={url}>{post.heading}</stencil-route-link>
-                    : post.heading
-                  }
-                </h1>,
+                <h4 class="project-item--heading">
+                  {!this.type ? (
+                    <stencil-route-link url={url}>
+                      {post.heading}
+                    </stencil-route-link>
+                  ) : (
+                    post.heading
+                  )}
+                </h4>,
               ]}
             />
           )}
           <div>
             {!this.type && (
               <transition-group
-                wrapper={'span'}
-                keys={(item) => (item.$ ? item.$.key : item.h ? item.h.key : item.$key$)}
-                config={{ duration: 550, timing: 'ease', delay: 150 }}
+                wrapper={"span"}
+                keys={(item) =>
+                  item.$ ? item.$.key : item.h ? item.h.key : item.$key$
+                }
+                config={{ duration: 550, timing: "ease", delay: 150 }}
                 mounted={this.ready}
                 trail
-                from={{ opacity: '0', transform: 'translateX(5px)' }}
-                enter={{ opacity: '1', transform: 'translateX(0px)' }}
-                leave={{ opacity: '0', transform: 'translateX(5px)' }}
-                items={[<p class="project-item--subheading">{post.subHeading}</p>]}
+                from={{ opacity: "0", transform: "translateX(5px)" }}
+                enter={{ opacity: "1", transform: "translateX(0px)" }}
+                leave={{ opacity: "0", transform: "translateX(5px)" }}
+                items={[
+                  <p class="project-item--subheading">{post.subHeading}</p>,
+                ]}
               />
             )}
 
-            {this.type === 'full-size' &&  (
+            {this.type === "full-size" && (
               <div itemprop="articleBody">
                 <section class="details">
                   <div innerHTML={post.details} />
                 </section>
                 {post.link && (
-                  <ui-button class="color--dark project-link" external icon={{}} href={post.link}>
+                  <ui-button
+                    class="color--dark project-link"
+                    external
+                    icon={{}}
+                    href={post.link}
+                  >
                     View
                   </ui-button>
                 )}
@@ -167,7 +186,7 @@ export class ProjectItem {
         </section>
       </article>
     ) : (
-      'loading'
+      "loading"
     );
   }
 }
