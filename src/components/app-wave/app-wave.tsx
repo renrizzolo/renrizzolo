767ad669b75672d50d6196a2674ac31210f71125
Ren Rizzolo <renrizmusic@gmail.com>
2023-08-31 16:14:59 +1000

    fix: wave perf

--- src/components/app-wave/app-wave.tsx
+++ src/components/app-wave/app-wave.tsx
@@ -1,13 +1,4 @@
-import {
-  Build,
-  Component,
-  Element,
-  Prop,
-  State,
-  Watch,
-  h,
-} from "@stencil/core";
-import Lottie from "lottie-web";
+import { Component, Element, Prop, State, h } from "@stencil/core";
 
 @Component({
   tag: "app-wave",
@@ -21,8 +12,17 @@ export class AppWave {
   @Element() el!: HTMLElement;
 
   toggleWave = () => {
-    this.disableWave = this.disableWave === "false" ? "true" : "false";
-    localStorage.setItem("disableWave", this.disableWave);
+    this.disableWave === "true" ? this.startWave() : this.stopWave();
+  };
+
+  stopWave = () => {
+    this.disableWave = "true";
+    localStorage.setItem("disableWave", "true");
+  };
+
+  startWave = () => {
+    this.disableWave = "false";
+    localStorage.setItem("disableWave", "false");
   };
 
   // just leaving this here...
@@ -41,18 +41,6 @@ export class AppWave {
   //     .getPropertyValue('--color-secondary')
   //     .replace('#', '%23');
   // }
-  @Watch("disableWave")
-  watchDisable(o) {
-    if (o === "true") {
-      Lottie.pause("wave-1");
-      Lottie.pause("wave-2");
-      Lottie.pause("wave-3");
-    } else {
-      Lottie.play("wave-1");
-      Lottie.play("wave-2");
-      Lottie.play("wave-3");
-    }
-  }
 
   componentDidLoad() {
     const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
@@ -71,52 +59,6 @@ export class AppWave {
         }
       });
     }
-
-    // thank u svg for being sandboxed 😏
-    // this.updateFillColor();
-    if (Build.isBrowser) {
-      // prettier-ignore
-      const data = { "v": "5.5.1", "fr": 24, "ip": 0, "op": 170, "w": 1920, "h": 1080, "nm": "Comp 1", "ddd": 0, "assets": [], "layers": [{ "ddd": 0, "ind": 3, "ty": 4, "nm": "wave Outlines", "sr": 1, "ks": { "o": { "a": 0, "k": 100, "ix": 11 }, "r": { "a": 0, "k": 0, "ix": 10 }, "p": { "a": 1, "k": [{ "i": { "x": 0.833, "y": 0.833 }, "o": { "x": 0.167, "y": 0.167 }, "t": 0, "s": [960, 759.75, 0], "to": [320.001, 0.042, 0], "ti": [-320.001, -0.042, 0] }, { "t": 171, "s": [2880.003, 760, 0] }], "ix": 2 }, "a": { "a": 0, "k": [150, 50, 0], "ix": 1 }, "s": { "a": 0, "k": [640, 640, 100], "ix": 6 } }, "ao": 0, "shapes": [{ "ty": "gr", "it": [{ "ind": 0, "ty": "sh", "ix": 1, "ks": { "a": 0, "k": { "i": [[0, 0], [0, 0], [0, 0], [26.5, -5.5], [47.203, 7.867], [0, 0]], "o": [[0, 0], [0, 0], [0, 0], [-44.683, 9.273], [-54, -9], [0, 0]], "v": [[-150, 32], [150, 32], [149.999, -10], [111.5, 0.5], [-33, -23], [-150.001, -10]], "c": true }, "ix": 2 }, "nm": "Path 1", "mn": "ADBE Vector Shape - Group", "hd": false }, { "ty": "st", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 3 }, "o": { "a": 0, "k": 100, "ix": 4 }, "w": { "a": 0, "k": 1, "ix": 5 }, "lc": 1, "lj": 1, "ml": 4, "bm": 0, "nm": "Stroke 2", "mn": "ADBE Vector Graphic - Stroke", "hd": false }, { "ty": "tr", "p": { "a": 0, "k": [150.079, 68.083], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [100, 100], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 6 }, "o": { "a": 0, "k": 100, "ix": 7 }, "sk": { "a": 0, "k": 0, "ix": 4 }, "sa": { "a": 0, "k": 0, "ix": 5 }, "nm": "Transform" }], "nm": "Group 1", "np": 7, "cix": 2, "bm": 0, "ix": 1, "mn": "ADBE Vector Group", "hd": false }, { "ty": "fl", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 4 }, "o": { "a": 0, "k": 100, "ix": 5 }, "r": 1, "bm": 0, "nm": "Fill 1", "mn": "ADBE Vector Graphic - Fill", "hd": false }, { "ty": "rp", "c": { "a": 0, "k": 2, "ix": 1 }, "o": { "a": 0, "k": 0, "ix": 2 }, "m": 2, "ix": 7, "tr": { "ty": "tr", "p": { "a": 0, "k": [-300, 0], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [100, 100], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 4 }, "so": { "a": 0, "k": 100, "ix": 5 }, "eo": { "a": 0, "k": 100, "ix": 6 }, "nm": "Transform" }, "nm": "Repeater 1", "mn": "ADBE Vector Filter - Repeater", "hd": false }], "ip": 0, "op": 600, "st": 0, "bm": 0 }], "markers": [] }
-      // prettier-ignore
-      const data2 = { "v": "5.5.1", "fr": 24, "ip": 0, "op": 170, "w": 1920, "h": 1080, "nm": "Comp 1", "ddd": 0, "assets": [], "layers": [{ "ddd": 0, "ind": 3, "ty": 4, "nm": "wave Outlines 2", "sr": 1, "ks": { "o": { "a": 0, "k": 100, "ix": 11 }, "r": { "a": 0, "k": 0, "ix": 10 }, "p": { "a": 1, "k": [{ "i": { "x": 0.833, "y": 0.833 }, "o": { "x": 0.167, "y": 0.167 }, "t": 0, "s": [960, 759.75, 0], "to": [320.001, 0.042, 0], "ti": [-320.001, -0.042, 0] }, { "t": 171, "s": [2880.003, 760, 0] }], "ix": 2 }, "a": { "a": 0, "k": [150, 50, 0], "ix": 1 }, "s": { "a": 0, "k": [640, 640, 100], "ix": 6 } }, "ao": 0, "shapes": [{ "ty": "gr", "it": [{ "ind": 0, "ty": "sh", "ix": 1, "ks": { "a": 0, "k": { "i": [[0, 0], [0, 0], [0, 0], [26.5, -5.5], [47.203, 7.867], [0, 0]], "o": [[0, 0], [0, 0], [0, 0], [-44.683, 9.273], [-54, -9], [0, 0]], "v": [[-150, 32], [150, 32], [149.999, -10], [111.5, 0.5], [-33, -23], [-150.001, -10]], "c": true }, "ix": 2 }, "nm": "Path 1", "mn": "ADBE Vector Shape - Group", "hd": false }, { "ty": "st", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 3 }, "o": { "a": 0, "k": 100, "ix": 4 }, "w": { "a": 0, "k": 1, "ix": 5 }, "lc": 1, "lj": 1, "ml": 4, "bm": 0, "nm": "Stroke 2", "mn": "ADBE Vector Graphic - Stroke", "hd": false }, { "ty": "tr", "p": { "a": 0, "k": [150.079, 68.083], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [100, 100], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 6 }, "o": { "a": 0, "k": 100, "ix": 7 }, "sk": { "a": 0, "k": 0, "ix": 4 }, "sa": { "a": 0, "k": 0, "ix": 5 }, "nm": "Transform" }], "nm": "Group 1", "np": 7, "cix": 2, "bm": 0, "ix": 1, "mn": "ADBE Vector Group", "hd": false }, { "ty": "fl", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 4 }, "o": { "a": 0, "k": 100, "ix": 5 }, "r": 1, "bm": 0, "nm": "Fill 1", "mn": "ADBE Vector Graphic - Fill", "hd": false }, { "ty": "rp", "c": { "a": 0, "k": 2, "ix": 1 }, "o": { "a": 0, "k": 0, "ix": 2 }, "m": 2, "ix": 7, "tr": { "ty": "tr", "p": { "a": 0, "k": [-300, 0], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [100, 100], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 4 }, "so": { "a": 0, "k": 100, "ix": 5 }, "eo": { "a": 0, "k": 100, "ix": 6 }, "nm": "Transform" }, "nm": "Repeater 1", "mn": "ADBE Vector Filter - Repeater", "hd": false }], "ip": 0, "op": 600, "st": 0, "bm": 0 }], "markers": [] }
-      // const data2 = { "v": "5.5.1", "fr": 24, "ip": 0, "op": 170, "w": 1920, "h": 1080, "nm": "Comp 1", "ddd": 0, "assets": [], "layers": [{ "ddd": 0, "ind": 3, "ty": 4, "nm": "wave 2", "sr": 1, "ks": { "o": { "a": 0, "k": 100, "ix": 11 }, "r": { "a": 0, "k": 0, "ix": 10 }, "p": { "a": 1, "k": [{ "i": { "x": 0.833, "y": 0.833 }, "o": { "x": 0.167, "y": 0.167 }, "t": 0, "s": [960, 759.75, 0], "to": [348.001, 88.042, 0], "ti": [-320.001, -0.042, 0] }, { "t": 171, "s": [2880.003, 760, 0] }], "ix": 2 }, "a": { "a": 0, "k": [150, 50, 0], "ix": 1 }, "s": { "a": 0, "k": [640, 640, 100], "ix": 6 } }, "ao": 0, "shapes": [{ "ty": "gr", "it": [{ "ind": 0, "ty": "sh", "ix": 1, "ks": { "a": 0, "k": { "i": [[0, 0], [0, 0], [0, 0], [26.5, -5.5], [47.203, 7.867], [0, 0]], "o": [[0, 0], [0, 0], [0, 0], [-44.683, 9.273], [-54, -9], [0, 0]], "v": [[-150, 32], [150, 32], [149.999, -10], [111.5, 0.5], [-33, -23], [-150.001, -10]], "c": true }, "ix": 2 }, "nm": "Path 1", "mn": "ADBE Vector Shape - Group", "hd": false }, { "ty": "st", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 3 }, "o": { "a": 0, "k": 100, "ix": 4 }, "w": { "a": 0, "k": 1, "ix": 5 }, "lc": 1, "lj": 1, "ml": 4, "bm": 0, "nm": "Stroke 2", "mn": "ADBE Vector Graphic - Stroke", "hd": false }, { "ty": "tr", "p": { "a": 0, "k": [150.079, 68.083], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [100, 100], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 6 }, "o": { "a": 0, "k": 100, "ix": 7 }, "sk": { "a": 0, "k": 0, "ix": 4 }, "sa": { "a": 0, "k": 0, "ix": 5 }, "nm": "Transform" }], "nm": "Group 1", "np": 7, "cix": 2, "bm": 0, "ix": 1, "mn": "ADBE Vector Group", "hd": false }, { "ty": "fl", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 4 }, "o": { "a": 0, "k": 100, "ix": 5 }, "r": 1, "bm": 0, "nm": "Fill 1", "mn": "ADBE Vector Graphic - Fill", "hd": false }, { "ty": "rp", "c": { "a": 0, "k": 2, "ix": 1 }, "o": { "a": 0, "k": 0, "ix": 2 }, "m": 2, "ix": 7, "tr": { "ty": "tr", "p": { "a": 0, "k": [-300, 0], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [100, 100], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 4 }, "so": { "a": 0, "k": 100, "ix": 5 }, "eo": { "a": 0, "k": 100, "ix": 6 }, "nm": "Transform" }, "nm": "Repeater 1", "mn": "ADBE Vector Filter - Repeater", "hd": false }], "ip": 0, "op": 600, "st": 0, "bm": 0 }], "markers": [] }
-      // prettier-ignore
-      const data3 = { "v": "5.5.1", "fr": 24, "ip": 0, "op": 170, "w": 1920, "h": 1080, "nm": "Comp 1", "ddd": 0, "assets": [], "layers": [{ "ddd": 0, "ind": 3, "ty": 4, "nm": "wave 3", "sr": 1, "ks": { "o": { "a": 0, "k": 100, "ix": 11 }, "r": { "a": 0, "k": 0, "ix": 10 }, "p": { "a": 1, "k": [{ "i": { "x": 0.562, "y": 0.312 }, "o": { "x": 0.189, "y": 0.249 }, "t": 0, "s": [960, 759.75, 0], "to": [136.521, 0.014, 0], "ti": [-291.499, -0.032, 0] }, { "i": { "x": 0.734, "y": 0.832 }, "o": { "x": 0.329, "y": 0.249 }, "t": 60, "s": [1627.955, 759.822, 0], "to": [444.856, 0.048, 0], "ti": [-389.327, -0.039, 0] }, { "i": { "x": 0.833, "y": 0.833 }, "o": { "x": 0.425, "y": 0.716 }, "t": 121, "s": [3042.908, 759.968, 0], "to": [137.287, 0.014, 0], "ti": [-73.468, -0.007, 0] }, { "t": 170, "s": [3470.003, 760, 0] }], "ix": 2 }, "a": { "a": 0, "k": [150, 50, 0], "ix": 1 }, "s": { "a": 0, "k": [640, 640, 100], "ix": 6 } }, "ao": 0, "shapes": [{ "ty": "gr", "it": [{ "ind": 0, "ty": "sh", "ix": 1, "ks": { "a": 0, "k": { "i": [[0, 0], [0, 0], [0, 0], [26.5, -5.5], [47.203, 7.867], [0, 0]], "o": [[0, 0], [0, 0], [0, 0], [-44.683, 9.273], [-54, -9], [0, 0]], "v": [[-150, 32], [150, 32], [149.999, -10], [111.5, 0.5], [-33, -23], [-150.001, -10]], "c": true }, "ix": 2 }, "nm": "Path 1", "mn": "ADBE Vector Shape - Group", "hd": false }, { "ty": "st", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 3 }, "o": { "a": 0, "k": 100, "ix": 4 }, "w": { "a": 0, "k": 1, "ix": 5 }, "lc": 1, "lj": 1, "ml": 4, "bm": 0, "nm": "Stroke 2", "mn": "ADBE Vector Graphic - Stroke", "hd": false }, { "ty": "tr", "p": { "a": 0, "k": [150.079, 80.083], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [131, 131], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 6 }, "o": { "a": 0, "k": 100, "ix": 7 }, "sk": { "a": 0, "k": 0, "ix": 4 }, "sa": { "a": 0, "k": 0, "ix": 5 }, "nm": "Transform" }], "nm": "Group 1", "np": 7, "cix": 2, "bm": 0, "ix": 1, "mn": "ADBE Vector Group", "hd": false }, { "ty": "fl", "c": { "a": 0, "k": [0, 0, 0, 1], "ix": 4 }, "o": { "a": 0, "k": 100, "ix": 5 }, "r": 1, "bm": 0, "nm": "Fill 1", "mn": "ADBE Vector Graphic - Fill", "hd": false }, { "ty": "rp", "c": { "a": 0, "k": 2, "ix": 1 }, "o": { "a": 0, "k": 0, "ix": 2 }, "m": 2, "ix": 7, "tr": { "ty": "tr", "p": { "a": 0, "k": [-393, 0], "ix": 2 }, "a": { "a": 0, "k": [0, 0], "ix": 1 }, "s": { "a": 0, "k": [100, 100], "ix": 3 }, "r": { "a": 0, "k": 0, "ix": 4 }, "so": { "a": 0, "k": 100, "ix": 5 }, "eo": { "a": 0, "k": 100, "ix": 6 }, "nm": "Transform" }, "nm": "Repeater 1", "mn": "ADBE Vector Filter - Repeater", "hd": false }], "ip": 0, "op": 600, "st": 0, "bm": 0 }], "markers": [] }
-
-      const elements = this.el.querySelectorAll(".wave");
-
-      Lottie.loadAnimation({
-        name: "wave-1",
-        container: elements[0],
-        renderer: "svg",
-        loop: true,
-        autoplay: this.disableWave === "false" && true,
-        animationData: data,
-      }).setSpeed(0.5);
-
-      Lottie.loadAnimation({
-        name: "wave-2",
-        container: elements[1],
-        renderer: "svg",
-        loop: true,
-        autoplay: this.disableWave === "false" && true,
-        animationData: data2,
-      }).setSpeed(0.45);
-
-      Lottie.loadAnimation({
-        name: "wave-3",
-        container: elements[2],
-        renderer: "svg",
-        loop: true,
-        autoplay: this.disableWave === "false" && true,
-        animationData: data3,
-      }).setSpeed(0.5);
-    }
-  }
-  componentWillUnload() {
-    Lottie.destroy("wave-1");
-    Lottie.destroy("wave-2");
-    Lottie.destroy("wave-3");
   }
 
   render() {
@@ -129,9 +71,77 @@ export class AppWave {
           {this.disableWave === "true" ? "Play" : "Staaaaahhhp"}
         </ui-button>
         <div class={"wave-container"}>
-          <div class={`wave ${this.class}`} />
-          <div class={`wave ${this.class}`} />
-          <div class={`wave ${this.class}`} />
+          {Array.from(Array(3)).map((_, i) => (
+            <div class={`wave ${this.class}`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 1920 1080"
+                width="1920"
+                height="1080"
+                preserveAspectRatio="xMidYMid meet"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  transform: "translate3d(0px, 0px, 0px)",
+                  contentVisibility: "visible",
+                }}
+              >
+                <g>
+                  <g
+                    class={`wave-animate-base wave-animate ${
+                      this.disableWave === "true" ? "wave-animate-paused" : ""
+                    } wave-${i + 1}`}
+                    opacity="1"
+                  >
+                    <g opacity="1" transform="matrix(1,0,0,1,0,0)">
+                      <path
+                        fill="rgb(0,0,0)"
+                        fill-opacity="1"
+                        d=" M0.08,100.08 C0.08,100.08 300.08,100.08 300.08,100.08 C300.08,100.08 300.08,58.08 300.08,58.08 C300.08,58.08 288.08,63.08 261.58,68.58 C216.9,77.86 164.28,52.95 117.08,45.08 C63.08,36.08 0.08,58.08 0.08,58.08 C0.08,58.08 0.08,100.08 0.08,100.08z"
+                      ></path>
+                      <g
+                        opacity="1"
+                        transform="matrix(1,0,0,1,150.07899475097656,68.08300018310547)"
+                      >
+                        <path
+                          stroke-linecap="butt"
+                          stroke-linejoin="miter"
+                          fill-opacity="0"
+                          stroke-miterlimit="4"
+                          stroke="rgb(0,0,0)"
+                          stroke-opacity="1"
+                          stroke-width="1"
+                          d=" M-150,32 C-150,32 150,32 150,32 C150,32 149.99899291992188,-10 149.99899291992188,-10 C149.99899291992188,-10 138,-5 111.5,0.5 C66.81700134277344,9.77299976348877 14.20300006866455,-15.133000373840332 -33,-23 C-87,-32 -150.00100708007812,-10 -150.00100708007812,-10 C-150.00100708007812,-10 -150,32 -150,32z"
+                        ></path>
+                      </g>
+                    </g>
+                    <g opacity="1" transform="matrix(1,0,0,1,-300,0)">
+                      <path
+                        fill="rgb(0,0,0)"
+                        fill-opacity="1"
+                        d=" M0.08,100.08 C0.08,100.08 300.08,100.08 300.08,100.08 C300.08,100.08 300.08,58.08 300.08,58.08 C300.08,58.08 288.08,63.08 261.58,68.58 C216.9,77.86 164.28,52.95 117.08,45.08 C63.08,36.08 0.08,58.08 0.08,58.08 C0.08,58.08 0.08,100.08 0.08,100.08z"
+                      ></path>
+                      <g
+                        opacity="1"
+                        transform="matrix(1,0,0,1,150.07899475097656,68.08300018310547)"
+                      >
+                        <path
+                          stroke-linecap="butt"
+                          stroke-linejoin="miter"
+                          fill-opacity="0"
+                          stroke-miterlimit="4"
+                          stroke="rgb(0,0,0)"
+                          stroke-opacity="1"
+                          stroke-width="1"
+                          d=" M-150,32 C-150,32 150,32 150,32 C150,32 149.99899291992188,-10 149.99899291992188,-10 C149.99899291992188,-10 138,-5 111.5,0.5 C66.81700134277344,9.77299976348877 14.20300006866455,-15.133000373840332 -33,-23 C-87,-32 -150.00100708007812,-10 -150.00100708007812,-10 C-150.00100708007812,-10 -150,32 -150,32z"
+                        ></path>
+                      </g>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </div>
+          ))}
         </div>
       </div>
     );
